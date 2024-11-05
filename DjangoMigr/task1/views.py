6ff76940071bb6a16c3aa30266f3ab9550989828
Vasiliy <v_derkach@inbox.ru>
2024-11-05 16:09:45 +0500

    0511

--- DjangoMigr/task1/views.py
+++ DjangoMigr/task1/views.py
@@ -56,11 +56,11 @@ def main(request):
     return render(request,"mainpage.html", context)
 def price(request):
     games=Game.objects.all()
-    page_lmt = 5
+    page_lmt = request.GET.get('page_limit')
     pagnt = Paginator(games,page_lmt)
     page_num = request.GET.get('page')
-    page_obj = paginator.get_page(page_num)
-    context = {'games': games, 'pagename': 'Игры','page_limit': page_lmt}
+    page_obj = pagnt.get_page(page_num)
+    context = {'games': games, 'pagename': 'Игры','page_limit': page_lmt,'page_obj': page_obj}
     return render(request,"price.html", context)
 def card(request):
     context = {'pagename': 'Корзина'}
